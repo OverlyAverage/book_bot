ab29ef7024f2ceeb19f35c7037062b54dae74e15
OverlyAverage <koheesion@gmail.com>
2025-07-11 18:18:47 +0100

    now imports sys and uses CL arguments instead of hard coded book path

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import word_count, char_count, sort_on
 
 def get_book_text(file_path):
@@ -24,7 +25,11 @@ def report(file_path, word_count, char_count):
 def main():
    # print(f"{word_count(get_book_text("books/frankenstein.txt"))} words found in the document.")
    # print(char_count(get_book_text('books/frankenstein.txt')))
-    report("books/frankenstein.txt",word_count(get_book_text("books/frankenstein.txt")), char_count(get_book_text("books/frankenstein.txt")))
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    report(sys.argv[1], word_count(get_book_text(sys.argv[1])), char_count(get_book_text(sys.argv[1])))
 
 main()
  
